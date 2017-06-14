7615493d21cbe31c902ca171e1c261a52201d161
Chaoyun1234 <dongchaoyun01@beyondsoft.com>
2017-06-14 02:57:05 -0700

    UpdateCrash

--- HelloAndroid/HelloAndroid/Resources/Resource.Designer.cs
+++ HelloAndroid/HelloAndroid/Resources/Resource.Designer.cs
@@ -40,6 +40,9 @@ namespace HelloAndroid
 			global::Microsoft.Azure.Mobile.Distribute.Resource.String.mobile_center_distribute_update_dialog_postpone = global::HelloAndroid.Resource.String.mobile_center_distribute_update_dialog_postpone;
 			global::Microsoft.Azure.Mobile.Distribute.Resource.String.mobile_center_distribute_update_dialog_title = global::HelloAndroid.Resource.String.mobile_center_distribute_update_dialog_title;
 			global::Microsoft.Azure.Mobile.Distribute.Resource.String.mobile_center_distribute_update_dialog_view_release_notes = global::HelloAndroid.Resource.String.mobile_center_distribute_update_dialog_view_release_notes;
+			global::Microsoft.Azure.Mobile.Push.Android.Resource.Integer.google_play_services_version = global::HelloAndroid.Resource.Integer.google_play_services_version;
+			global::Microsoft.Azure.Mobile.Push.Android.Resource.String.common_google_play_services_unknown_issue = global::HelloAndroid.Resource.String.common_google_play_services_unknown_issue;
+			global::Microsoft.Azure.Mobile.Push.Android.Resource.String.library_name = global::HelloAndroid.Resource.String.library_name;
 		}
 		
 		public partial class Attribute
@@ -125,56 +128,59 @@ namespace HelloAndroid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050010
-			public const int ApplicationName = 2131034128;
+			// aapt resource value: 0x7f050011
+			public const int ApplicationName = 2131034129;
 			
-			// aapt resource value: 0x7f05000f
-			public const int Hello = 2131034127;
+			// aapt resource value: 0x7f050010
+			public const int Hello = 2131034128;
 			
 			// aapt resource value: 0x7f050000
 			public const int common_google_play_services_unknown_issue = 2131034112;
 			
 			// aapt resource value: 0x7f050001
-			public const int mobile_center_distribute_dialog_actioned_on_disabled_toast = 2131034113;
+			public const int library_name = 2131034113;
 			
 			// aapt resource value: 0x7f050002
-			public const int mobile_center_distribute_download_progress_number_format = 2131034114;
+			public const int mobile_center_distribute_dialog_actioned_on_disabled_toast = 2131034114;
 			
 			// aapt resource value: 0x7f050003
-			public const int mobile_center_distribute_downloading_mandatory_update = 2131034115;
+			public const int mobile_center_distribute_download_progress_number_format = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int mobile_center_distribute_install = 2131034116;
+			public const int mobile_center_distribute_downloading_mandatory_update = 2131034116;
 			
 			// aapt resource value: 0x7f050005
-			public const int mobile_center_distribute_install_ready_message = 2131034117;
+			public const int mobile_center_distribute_install = 2131034117;
 			
 			// aapt resource value: 0x7f050006
-			public const int mobile_center_distribute_install_ready_title = 2131034118;
+			public const int mobile_center_distribute_install_ready_message = 2131034118;
 			
 			// aapt resource value: 0x7f050007
-			public const int mobile_center_distribute_unknown_sources_dialog_message = 2131034119;
+			public const int mobile_center_distribute_install_ready_title = 2131034119;
 			
 			// aapt resource value: 0x7f050008
-			public const int mobile_center_distribute_unknown_sources_dialog_settings = 2131034120;
+			public const int mobile_center_distribute_unknown_sources_dialog_message = 2131034120;
 			
 			// aapt resource value: 0x7f050009
-			public const int mobile_center_distribute_update_dialog_download = 2131034121;
+			public const int mobile_center_distribute_unknown_sources_dialog_settings = 2131034121;
 			
 			// aapt resource value: 0x7f05000a
-			public const int mobile_center_distribute_update_dialog_message_mandatory = 2131034122;
+			public const int mobile_center_distribute_update_dialog_download = 2131034122;
 			
 			// aapt resource value: 0x7f05000b
-			public const int mobile_center_distribute_update_dialog_message_optional = 2131034123;
+			public const int mobile_center_distribute_update_dialog_message_mandatory = 2131034123;
 			
 			// aapt resource value: 0x7f05000c
-			public const int mobile_center_distribute_update_dialog_postpone = 2131034124;
+			public const int mobile_center_distribute_update_dialog_message_optional = 2131034124;
 			
 			// aapt resource value: 0x7f05000d
-			public const int mobile_center_distribute_update_dialog_title = 2131034125;
+			public const int mobile_center_distribute_update_dialog_postpone = 2131034125;
 			
 			// aapt resource value: 0x7f05000e
-			public const int mobile_center_distribute_update_dialog_view_release_notes = 2131034126;
+			public const int mobile_center_distribute_update_dialog_title = 2131034126;
+			
+			// aapt resource value: 0x7f05000f
+			public const int mobile_center_distribute_update_dialog_view_release_notes = 2131034127;
 			
 			static String()
 			{
--- HelloAndroid/HelloAndroid/obj/Debug/android/com/microsoft/azure/mobile/R.java
+++ HelloAndroid/HelloAndroid/obj/Debug/android/com/microsoft/azure/mobile/R.java
@@ -24,22 +24,23 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int ApplicationName=0x7f050010;
-        public static final int Hello=0x7f05000f;
+        public static final int ApplicationName=0x7f050011;
+        public static final int Hello=0x7f050010;
         public static final int common_google_play_services_unknown_issue=0x7f050000;
-        public static final int mobile_center_distribute_dialog_actioned_on_disabled_toast=0x7f050001;
-        public static final int mobile_center_distribute_download_progress_number_format=0x7f050002;
-        public static final int mobile_center_distribute_downloading_mandatory_update=0x7f050003;
-        public static final int mobile_center_distribute_install=0x7f050004;
-        public static final int mobile_center_distribute_install_ready_message=0x7f050005;
-        public static final int mobile_center_distribute_install_ready_title=0x7f050006;
-        public static final int mobile_center_distribute_unknown_sources_dialog_message=0x7f050007;
-        public static final int mobile_center_distribute_unknown_sources_dialog_settings=0x7f050008;
-        public static final int mobile_center_distribute_update_dialog_download=0x7f050009;
-        public static final int mobile_center_distribute_update_dialog_message_mandatory=0x7f05000a;
-        public static final int mobile_center_distribute_update_dialog_message_optional=0x7f05000b;
-        public static final int mobile_center_distribute_update_dialog_postpone=0x7f05000c;
-        public static final int mobile_center_distribute_update_dialog_title=0x7f05000d;
-        public static final int mobile_center_distribute_update_dialog_view_release_notes=0x7f05000e;
+        public static final int library_name=0x7f050001;
+        public static final int mobile_center_distribute_dialog_actioned_on_disabled_toast=0x7f050002;
+        public static final int mobile_center_distribute_download_progress_number_format=0x7f050003;
+        public static final int mobile_center_distribute_downloading_mandatory_update=0x7f050004;
+        public static final int mobile_center_distribute_install=0x7f050005;
+        public static final int mobile_center_distribute_install_ready_message=0x7f050006;
+        public static final int mobile_center_distribute_install_ready_title=0x7f050007;
+        public static final int mobile_center_distribute_unknown_sources_dialog_message=0x7f050008;
+        public static final int mobile_center_distribute_unknown_sources_dialog_settings=0x7f050009;
+        public static final int mobile_center_distribute_update_dialog_download=0x7f05000a;
+        public static final int mobile_center_distribute_update_dialog_message_mandatory=0x7f05000b;
+        public static final int mobile_center_distribute_update_dialog_message_optional=0x7f05000c;
+        public static final int mobile_center_distribute_update_dialog_postpone=0x7f05000d;
+        public static final int mobile_center_distribute_update_dialog_title=0x7f05000e;
+        public static final int mobile_center_distribute_update_dialog_view_release_notes=0x7f05000f;
     }
 }
