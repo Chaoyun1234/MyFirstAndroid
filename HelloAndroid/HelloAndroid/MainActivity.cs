9950dd5d33e0ac76cdc5e9c1964ac82a2e361658
Chaoyun1234 <dongchaoyun01@beyondsoft.com>
2017-06-28 04:46:57 -0700

    UpdatePushInfo

--- HelloAndroid/HelloAndroid/MainActivity.cs
+++ HelloAndroid/HelloAndroid/MainActivity.cs
@@ -5,6 +5,7 @@ using Microsoft.Azure.Mobile;
 using Microsoft.Azure.Mobile.Analytics;
 using Microsoft.Azure.Mobile.Crashes;
 using Microsoft.Azure.Mobile.Distribute;
+using Microsoft.Azure.Mobile.Push;
 
 namespace HelloAndroid
 {
@@ -14,13 +15,48 @@ namespace HelloAndroid
         int count = 0;
         protected override void OnCreate(Bundle bundle)
         {
+            MobileCenter.LogLevel = LogLevel.Verbose;
+            Push.Enabled = true;
+            MobileCenter.Start("667f0dc5-7c80-4a31-a851-78c2fc4abd93",
+                   typeof(Analytics), typeof(Crashes), typeof(Distribute), typeof(Push));
+            var installid = MobileCenter.InstallId;
+
+            Push.PushNotificationReceived += (sender, e) => {
+
+                // Add the notification message and title to the message
+                var summary = $"Push notification received:" +
+                        $"\n\tNotification title: {e.Title}" +
+                        $"\n\tMessage: {e.Message}";
+
+                // If there is custom data associated with the notification,
+                // print the entries
+                if (e.CustomData != null)
+                {
+                    summary += "\n\tCustom data:\n";
+                    foreach (var key in e.CustomData.Keys)
+                    {
+                        summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                    }
+                }
+
+                // Send the notification summary to debug output
+                System.Diagnostics.Debug.WriteLine(summary);
+            };
+            // 绑定 Click 事件
+
+
+
+            Analytics.TrackEvent("Click");
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+
             base.OnCreate(bundle);
             // 加载布局
             SetContentView(Resource.Layout.Main);
             // 获取布局中的控件
             Button say = FindViewById<Button>(Resource.Id.sayHello);
             TextView show = FindViewById<TextView>(Resource.Id.showHello);
-            // 绑定 Click 事件
             say.Click += (sender, e) =>
             {
                 count++;
@@ -30,22 +66,14 @@ namespace HelloAndroid
                 if (count > 5)
                 {
                     Crashes.Enabled = true;
-                    //throw new System.Exception("error:click>5");
-                    Crashes.GenerateTestCrash();
+                    throw new System.Exception("error:click>5");
+                    //Crashes.GenerateTestCrash();
                 }
 
                 // Toast 通知
                 Toast.MakeText(this, $"You Clicked {count}", ToastLength.Short).Show();
             };
 
-
-            MobileCenter.Start("667f0dc5-7c80-4a31-a851-78c2fc4abd93",
-                   typeof(Analytics), typeof(Crashes), typeof(Distribute));
-
-            Analytics.TrackEvent("Click");
-
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
         }
     }
 }
--- HelloAndroid/HelloAndroid/Resources/Resource.Designer.cs
+++ HelloAndroid/HelloAndroid/Resources/Resource.Designer.cs
@@ -48,6 +48,24 @@ namespace HelloAndroid
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010003
+			public const int buttonSize = 2130771971;
+			
+			// aapt resource value: 0x7f010002
+			public const int circleCrop = 2130771970;
+			
+			// aapt resource value: 0x7f010004
+			public const int colorScheme = 2130771972;
+			
+			// aapt resource value: 0x7f010001
+			public const int imageAspectRatio = 2130771969;
+			
+			// aapt resource value: 0x7f010000
+			public const int imageAspectRatioAdjust = 2130771968;
+			
+			// aapt resource value: 0x7f010005
+			public const int scopeUris = 2130771973;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -58,11 +76,120 @@ namespace HelloAndroid
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040008
+			public const int common_google_signin_btn_text_dark = 2130968584;
+			
+			// aapt resource value: 0x7f040000
+			public const int common_google_signin_btn_text_dark_default = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int common_google_signin_btn_text_dark_disabled = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int common_google_signin_btn_text_dark_focused = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int common_google_signin_btn_text_dark_pressed = 2130968579;
+			
+			// aapt resource value: 0x7f040009
+			public const int common_google_signin_btn_text_light = 2130968585;
+			
+			// aapt resource value: 0x7f040004
+			public const int common_google_signin_btn_text_light_default = 2130968580;
+			
+			// aapt resource value: 0x7f040005
+			public const int common_google_signin_btn_text_light_disabled = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int common_google_signin_btn_text_light_focused = 2130968582;
+			
+			// aapt resource value: 0x7f040007
+			public const int common_google_signin_btn_text_light_pressed = 2130968583;
+			
+			// aapt resource value: 0x7f04000a
+			public const int common_google_signin_btn_tint = 2130968586;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int common_full_open_on_phone = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int common_google_signin_btn_icon_dark = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int common_google_signin_btn_icon_dark_focused = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int common_google_signin_btn_icon_dark_normal = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int common_google_signin_btn_icon_dark_normal_background = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int common_google_signin_btn_icon_disabled = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int common_google_signin_btn_icon_light = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int common_google_signin_btn_icon_light_focused = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int common_google_signin_btn_icon_light_normal = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int common_google_signin_btn_icon_light_normal_background = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int common_google_signin_btn_text_dark = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int common_google_signin_btn_text_dark_focused = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int common_google_signin_btn_text_dark_normal = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int common_google_signin_btn_text_dark_normal_background = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int common_google_signin_btn_text_disabled = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int common_google_signin_btn_text_light = 2130837519;
+			
+			// aapt resource value: 0x7f020010
+			public const int common_google_signin_btn_text_light_focused = 2130837520;
+			
+			// aapt resource value: 0x7f020011
+			public const int common_google_signin_btn_text_light_normal = 2130837521;
+			
+			// aapt resource value: 0x7f020012
+			public const int common_google_signin_btn_text_light_normal_background = 2130837522;
+			
+			// aapt resource value: 0x7f020013
+			public const int googleg_disabled_color_18 = 2130837523;
+			
+			// aapt resource value: 0x7f020014
+			public const int googleg_standard_color_18 = 2130837524;
+			
+			// aapt resource value: 0x7f020015
+			public const int Icon = 2130837525;
 			
 			static Drawable()
 			{
@@ -77,11 +204,38 @@ namespace HelloAndroid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060001
-			public const int sayHello = 2131099649;
+			// aapt resource value: 0x7f070000
+			public const int adjust_height = 2131165184;
 			
-			// aapt resource value: 0x7f060000
-			public const int showHello = 2131099648;
+			// aapt resource value: 0x7f070001
+			public const int adjust_width = 2131165185;
+			
+			// aapt resource value: 0x7f070006
+			public const int auto = 2131165190;
+			
+			// aapt resource value: 0x7f070007
+			public const int dark = 2131165191;
+			
+			// aapt resource value: 0x7f070003
+			public const int icon_only = 2131165187;
+			
+			// aapt resource value: 0x7f070008
+			public const int light = 2131165192;
+			
+			// aapt resource value: 0x7f070002
+			public const int none = 2131165186;
+			
+			// aapt resource value: 0x7f07000a
+			public const int sayHello = 2131165194;
+			
+			// aapt resource value: 0x7f070009
+			public const int showHello = 2131165193;
+			
+			// aapt resource value: 0x7f070004
+			public const int standard = 2131165188;
+			
+			// aapt resource value: 0x7f070005
+			public const int wide = 2131165189;
 			
 			static Id()
 			{
@@ -96,8 +250,8 @@ namespace HelloAndroid
 		public partial class Integer
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int google_play_services_version = 2130968576;
+			// aapt resource value: 0x7f060000
+			public const int google_play_services_version = 2131099648;
 			
 			static Integer()
 			{
@@ -128,59 +282,107 @@ namespace HelloAndroid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050011
-			public const int ApplicationName = 2131034129;
+			// aapt resource value: 0x7f050021
+			public const int ApplicationName = 2131034145;
 			
-			// aapt resource value: 0x7f050010
-			public const int Hello = 2131034128;
+			// aapt resource value: 0x7f050020
+			public const int Hello = 2131034144;
 			
 			// aapt resource value: 0x7f050000
-			public const int common_google_play_services_unknown_issue = 2131034112;
+			public const int common_google_play_services_enable_button = 2131034112;
 			
 			// aapt resource value: 0x7f050001
-			public const int library_name = 2131034113;
+			public const int common_google_play_services_enable_text = 2131034113;
 			
 			// aapt resource value: 0x7f050002
-			public const int mobile_center_distribute_dialog_actioned_on_disabled_toast = 2131034114;
+			public const int common_google_play_services_enable_title = 2131034114;
 			
 			// aapt resource value: 0x7f050003
-			public const int mobile_center_distribute_download_progress_number_format = 2131034115;
+			public const int common_google_play_services_install_button = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int mobile_center_distribute_downloading_mandatory_update = 2131034116;
+			public const int common_google_play_services_install_text = 2131034116;
 			
 			// aapt resource value: 0x7f050005
-			public const int mobile_center_distribute_install = 2131034117;
+			public const int common_google_play_services_install_title = 2131034117;
 			
 			// aapt resource value: 0x7f050006
-			public const int mobile_center_distribute_install_ready_message = 2131034118;
+			public const int common_google_play_services_notification_ticker = 2131034118;
+			
+			// aapt resource value: 0x7f050010
+			public const int common_google_play_services_unknown_issue = 2131034128;
 			
 			// aapt resource value: 0x7f050007
-			public const int mobile_center_distribute_install_ready_title = 2131034119;
+			public const int common_google_play_services_unsupported_text = 2131034119;
 			
 			// aapt resource value: 0x7f050008
-			public const int mobile_center_distribute_unknown_sources_dialog_message = 2131034120;
+			public const int common_google_play_services_update_button = 2131034120;
 			
 			// aapt resource value: 0x7f050009
-			public const int mobile_center_distribute_unknown_sources_dialog_settings = 2131034121;
+			public const int common_google_play_services_update_text = 2131034121;
 			
 			// aapt resource value: 0x7f05000a
-			public const int mobile_center_distribute_update_dialog_download = 2131034122;
+			public const int common_google_play_services_update_title = 2131034122;
 			
 			// aapt resource value: 0x7f05000b
-			public const int mobile_center_distribute_update_dialog_message_mandatory = 2131034123;
+			public const int common_google_play_services_updating_text = 2131034123;
 			
 			// aapt resource value: 0x7f05000c
-			public const int mobile_center_distribute_update_dialog_message_optional = 2131034124;
+			public const int common_google_play_services_wear_update_text = 2131034124;
 			
 			// aapt resource value: 0x7f05000d
-			public const int mobile_center_distribute_update_dialog_postpone = 2131034125;
+			public const int common_open_on_phone = 2131034125;
 			
 			// aapt resource value: 0x7f05000e
-			public const int mobile_center_distribute_update_dialog_title = 2131034126;
+			public const int common_signin_button_text = 2131034126;
 			
 			// aapt resource value: 0x7f05000f
-			public const int mobile_center_distribute_update_dialog_view_release_notes = 2131034127;
+			public const int common_signin_button_text_long = 2131034127;
+			
+			// aapt resource value: 0x7f050011
+			public const int library_name = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int mobile_center_distribute_dialog_actioned_on_disabled_toast = 2131034130;
+			
+			// aapt resource value: 0x7f050013
+			public const int mobile_center_distribute_download_progress_number_format = 2131034131;
+			
+			// aapt resource value: 0x7f050014
+			public const int mobile_center_distribute_downloading_mandatory_update = 2131034132;
+			
+			// aapt resource value: 0x7f050015
+			public const int mobile_center_distribute_install = 2131034133;
+			
+			// aapt resource value: 0x7f050016
+			public const int mobile_center_distribute_install_ready_message = 2131034134;
+			
+			// aapt resource value: 0x7f050017
+			public const int mobile_center_distribute_install_ready_title = 2131034135;
+			
+			// aapt resource value: 0x7f050018
+			public const int mobile_center_distribute_unknown_sources_dialog_message = 2131034136;
+			
+			// aapt resource value: 0x7f050019
+			public const int mobile_center_distribute_unknown_sources_dialog_settings = 2131034137;
+			
+			// aapt resource value: 0x7f05001a
+			public const int mobile_center_distribute_update_dialog_download = 2131034138;
+			
+			// aapt resource value: 0x7f05001b
+			public const int mobile_center_distribute_update_dialog_message_mandatory = 2131034139;
+			
+			// aapt resource value: 0x7f05001c
+			public const int mobile_center_distribute_update_dialog_message_optional = 2131034140;
+			
+			// aapt resource value: 0x7f05001d
+			public const int mobile_center_distribute_update_dialog_postpone = 2131034141;
+			
+			// aapt resource value: 0x7f05001e
+			public const int mobile_center_distribute_update_dialog_title = 2131034142;
+			
+			// aapt resource value: 0x7f05001f
+			public const int mobile_center_distribute_update_dialog_view_release_notes = 2131034143;
 			
 			static String()
 			{
@@ -188,6 +390,47 @@ namespace HelloAndroid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] LoadingImageView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970};
+			
+			// aapt resource value: 2
+			public const int LoadingImageView_circleCrop = 2;
+			
+			// aapt resource value: 1
+			public const int LoadingImageView_imageAspectRatio = 1;
+			
+			// aapt resource value: 0
+			public const int LoadingImageView_imageAspectRatioAdjust = 0;
+			
+			public static int[] SignInButton = new int[] {
+					2130771971,
+					2130771972,
+					2130771973};
+			
+			// aapt resource value: 0
+			public const int SignInButton_buttonSize = 0;
+			
+			// aapt resource value: 1
+			public const int SignInButton_colorScheme = 1;
+			
+			// aapt resource value: 2
+			public const int SignInButton_scopeUris = 2;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
